b3784463de3eb8540cfd25d2f82c1aad2c218356
anton <anton_panoff@msn.com>
2023-03-31 06:12:03 +0300

    begin

---
+++ src/main/java/com/github/Panant76/TrackLogisticsDemo/TrackLogisticsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.Panant76.TrackLogisticsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackLogisticsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackLogisticsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Panant76/TrackLogisticsDemo/TrackLogisticsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Panant76.TrackLogisticsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackLogisticsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
